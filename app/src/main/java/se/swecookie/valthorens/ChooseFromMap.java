fba9daf60d80eae7611b9516d8e7f099a93115a3
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-03-27 19:39:50 +0200

    Map touch fixed

--- app/src/main/java/se/swecookie/valthorens/ChooseFromMap.java
+++ app/src/main/java/se/swecookie/valthorens/ChooseFromMap.java
@@ -9,6 +9,7 @@ import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,6 @@ import com.squareup.picasso.Picasso;
 public class ChooseFromMap extends AppCompatActivity {
     private ImageView imgMap;
     private TextView txtCoordinates;
-    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,82 +29,46 @@ public class ChooseFromMap extends AppCompatActivity {
 
         imgMap = (ImageView) findViewById(R.id.imgMap);
         txtCoordinates = (TextView) findViewById(R.id.txtCoordinates);
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn8 = (Button) findViewById(R.id.btn8);
 
         Picasso.with(ChooseFromMap.this)
                 .load(R.drawable.map3)
                 .resize(1067, 489) // half size
                 .into(imgMap);
 
-        setButtonCoordinates();
-
         imgMap.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    double posX = (int) event.getX();
-                    //int posY = (int) event.getY();
-                    txtCoordinates.setText("Touch coordinates : " + String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+                    double posX = event.getX();
+                    txtCoordinates.setText("Touch coordinates : " + posX + "x" + event.getY());
                     double maxWidth = imgMap.getWidth();
                     double touchedXPercentage = posX / maxWidth;
-                    /*if (checkConnection()) {
-                        if (touchedXPercentage < 0.15) { //Punkt 1
+                    if (checkConnection()) {
+                        if (touchedXPercentage < 0.1102) { //Funitel 3 vallees
                             MainActivity.clickedImageNumber = 1;
-                            startActivity(new Intent(ChooseFromMap.this, Webcam.class));
-                            //finish();
-                        } else if (touchedXPercentage < 0.33) { //Punkt 2
+                        } else if (touchedXPercentage < 0.2) { //Plein sud
+                            MainActivity.clickedImageNumber = 8;
+                        } else if (touchedXPercentage < 0.276) { //De la maison
                             MainActivity.clickedImageNumber = 2;
-                            startActivity(new Intent(ChooseFromMap.this, Webcam.class));
-                            //finish();
-                        } else if (touchedXPercentage < 0.52) { //Punkt 3
+                        } else if (touchedXPercentage < 0.3625) { //Livecam 360
+                            MainActivity.clickedImageNumber = 7;
+                        } else if (touchedXPercentage < 0.54) { //Les 2 lacs
                             MainActivity.clickedImageNumber = 3;
-                            startActivity(new Intent(ChooseFromMap.this, Webcam.class));
-                            //finish();
-                        } else { //Punkt 4
+                        } else if (touchedXPercentage < 0.693) { //Funitel de thorens
                             MainActivity.clickedImageNumber = 4;
-                            startActivity(new Intent(ChooseFromMap.this, Webcam.class));
-                            //finish();
+                        } else if (touchedXPercentage < 0.83) { //La tryolienne
+                            MainActivity.clickedImageNumber = 5;
+                        } else {    //Plan Bouchet
+                            MainActivity.clickedImageNumber = 6;
                         }
+                        startActivity(new Intent(ChooseFromMap.this, Webcam.class));
                     } else {
                         showConnectionError();
-                    }*/
+                    }
                 }
                 return true;
             }
         });
-    }
-
-    private void setButtonCoordinates() {
-        btn1.setX(55);
-        btn1.setY(615);
-
-        btn2.setX(295);
-        btn2.setY(615);
-
-        btn3.setX(499);
-        btn3.setY(727);
-
-        btn4.setX(655);
-        btn4.setY(720);
-
-        btn5.setX(920);
-        btn5.setY(727);
-
-        btn6.setX(1430);
-        btn6.setY(320);
-
-        btn7.setX(1720);
-        btn7.setY(160);
-
-        btn8.setX(2120);
-        btn8.setY(710);
     }
 
     private void showConnectionError() {
@@ -139,13 +103,10 @@ public class ChooseFromMap extends AppCompatActivity {
         return displayMetrics.heightPixels;
     }
 
+    int getWidth() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
 }
-
-
-/*
-MAX X: 2390
-
-PUNKT 1 X: 142
-    MITTEN = 590+142 / 2 ==== 366 == 0.15%
-
- */
