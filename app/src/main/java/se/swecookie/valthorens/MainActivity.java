d181e55dc9f9e5fea4c2b97fca6ce513bdde810e
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-12-12 10:30:21 +0100

    1.2 Started adding ads

--- app/src/main/java/se/swecookie/valthorens/MainActivity.java
+++ app/src/main/java/se/swecookie/valthorens/MainActivity.java
@@ -10,14 +10,59 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import se.swecookie.valthorens.helper.Links;
 
 public class MainActivity extends AppCompatActivity {
     public static Webcam clickedImageNumber = Webcam.FUNITEL_DE_THORENS;
+
+    private ImageView funitel_3_vallees, de_la_maison, les_2_lacs, funitel_de_thorens, la_tyrolienne, plan_bouchet, livecam_360, plein_sud, tsd_moutiere, cime_caron;
+    private ProgressBar progressBar;
+
+    private int loadedCount;
+    private static final int TOTAL_COUNT = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        getImages();
+    }
+
+    // TODO App-id: ca-app-pub-2831297200743176~1134333565
+    // TODO Ad-unit-id: ca-app-pub-2831297200743176/9525504068
+
+    private void init() {
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        funitel_3_vallees = findViewById(R.id.funitel_3_vallees);
+        de_la_maison = findViewById(R.id.de_la_maison);
+        les_2_lacs = findViewById(R.id.les_2_lacs);
+        funitel_de_thorens = findViewById(R.id.funitel_de_thorens);
+        la_tyrolienne = findViewById(R.id.la_tyrolienne);
+        plan_bouchet = findViewById(R.id.plan_bouchet);
+        livecam_360 = findViewById(R.id.livecam_360);
+        plein_sud = findViewById(R.id.plein_sud);
+        tsd_moutiere = findViewById(R.id.tsd_moutiere);
+        cime_caron = findViewById(R.id.cime_caron);
+        progressBar = findViewById(R.id.progressBar);
+        loadedCount = 0;
     }
 
     public void onClick(View view) {
@@ -85,6 +130,169 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
+    }
+
+    private void getImages() {
+        Picasso.with(this)
+                .load(Links.FUNITEL_3_VALLEES)
+                .into(funitel_3_vallees, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.DE_LA_MAISON)
+                .into(de_la_maison, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.LES_2_LACS)
+                .into(les_2_lacs, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.FUNITEL_DE_THORENS)
+                .into(funitel_de_thorens, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.LA_TYROLIENNE)
+                .into(la_tyrolienne, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.PLAN_BOUCHET)
+                .into(plan_bouchet, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.LIVECAM_360)
+                .into(livecam_360, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.PLEIN_SUD)
+                .into(plein_sud, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.TSD_MOUTIERE)
+                .into(tsd_moutiere, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        Picasso.with(this)
+                .load(Links.CIME_CARON)
+                .into(cime_caron, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        loadedCount++;
+                        if (loadedCount >= TOTAL_COUNT) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
     }
 
     private boolean checkConnection() {
---
+++ app/src/main/java/se/swecookie/valthorens/helper/Links.java
@@ -0,0 +1,17 @@
+package se.swecookie.valthorens.helper;
+
+import android.net.Uri;
+
+public class Links {
+    public static final Uri FUNITEL_3_VALLEES = Uri.parse("http://swecookie.se/apps/valthorens/funitel_3_vallees.png");
+    public static final Uri DE_LA_MAISON = Uri.parse("http://swecookie.se/apps/valthorens/de_la_maison.png");
+    public static final Uri LES_2_LACS = Uri.parse("http://swecookie.se/apps/valthorens/les_2_lacs.png");
+    public static final Uri FUNITEL_DE_THORENS = Uri.parse("http://swecookie.se/apps/valthorens/funitel_de_thorens.png");
+    public static final Uri LA_TYROLIENNE = Uri.parse("http://swecookie.se/apps/valthorens/la_tyrolienne.png");
+    public static final Uri PLAN_BOUCHET = Uri.parse("http://swecookie.se/apps/valthorens/plan_bouchet.png");
+    public static final Uri LIVECAM_360 = Uri.parse("http://swecookie.se/apps/valthorens/livecam_360.png");
+    public static final Uri PLEIN_SUD = Uri.parse("http://swecookie.se/apps/valthorens/plein_sud.png");
+    public static final Uri TSD_MOUTIERE = Uri.parse("http://swecookie.se/apps/valthorens/tsd_moutiere.png");
+    public static final Uri CIME_CARON = Uri.parse("http://swecookie.se/apps/valthorens/cime_caron.png");
+
+}
